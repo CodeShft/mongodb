9ec1481c8c0896aa869a0578fb2033fafa4e094d
CodeShft <nileceyazilim@gmail.com>
2025-01-06 23:38:37 +0300

    feat: Added new features, fixed issues, and updated the icon.

--- mongodbprisma/app/components/navbar/Navbar.tsx
+++ mongodbprisma/app/components/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between gap-3 md:gap-10 px-3 md:px-10 h-16 bg-orange-600">
       <Logo />
-      <Search />
+      <Search/>
       <CardCount />
       <User />
       <HamburgerMenu />
