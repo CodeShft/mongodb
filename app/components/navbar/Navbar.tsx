b38d861e8b269190d84a9b1db7115bdba97d09bb
CodeShft <nileceyazilim@gmail.com>
2025-01-07 22:33:33 +0300

    Updated navbar and search functionality.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,18 +4,23 @@ import CardCount from "./CardCount";
 import User from "./User";
 import HamburgerMenu from "./HamburgerMenu";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <div className="flex items-center justify-between gap-0 md:gap-4 px-3 md:px-10 h-16 bg-orange-600">
       <Logo />
 
       <div className="hidden md:flex gap-4 items-center">
-        <Search />
+        <div className="opacity-90">
+          <Search />
+        </div>
         <CardCount />
         <User />
       </div>
 
-      <div className="md:hidden flex items-center">
+      <div className="md:hidden flex items-center gap-2">
+        <div className="opacity-90">
+          <Search />
+        </div>
         <HamburgerMenu />
       </div>
     </div>
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,6 +1,33 @@
-"use client"
-const Search = () => {
-  return <div className="flex flex-1">Search</div>;
+"use client";
+import { useState } from "react";
+
+const Search: React.FC = () => {
+  const [query, setQuery] = useState<string>("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Arama Terimi: ", query);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center w-full md:w-auto relative"
+    >
+      <input
+        className="form-control py-2 px-4 rounded-lg focus:outline-none w-full sm:w-auto mr-4"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        value={query}
+        onChange={handleInputChange}
+      />
+    </form>
+  );
 };
 
 export default Search;
