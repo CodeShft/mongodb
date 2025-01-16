a59960d8040d90f0f225dabad5fd7a17d1260538
CodeShft <nileceyazilim@gmail.com>
2025-01-16 05:11:21 +0300

    fixed footer problem.

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,8 @@
 "use client";
+
 const Footer = () => {
   return (
-    <footer className="w-full bg-orange-600 text-white py-4 h-16 mt-auto flex items-center justify-center">
+    <footer className="w-full bg-orange-600 text-white py-4 h-16 mt-4">
       <div className="container mx-auto text-center text-base">
         <p>&copy; 2025 My Website. All rights reserved.</p>
       </div>
