84e13ef6b15d3a5060b4448f38bb9a6b2f801386
CodeShft <nileceyazilim@gmail.com>
2025-02-07 14:52:27 +0300

    feat: added login screen

---
+++ app/login/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Logging in with:', { email, password });
+  };
+
+  return (
+    <div className="flex items-center justify-center px-4">
+      <div className="bg-yellow-300 p-6 rounded-lg shadow-lg w-full sm:max-w-md mt-16"> {/* mt-16 ile biraz daha aşağıya alındı */}
+
+        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-800">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-2 p-3 w-full border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="mt-2 p-3 w-full border rounded-md focus:outline-none focus:ring-2"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-orange-600 text-white py-3 rounded-md hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500 transition duration-300"
+          >
+            Login
+          </button>
+        </form>
+
+        <div className="mt-4 text-center">
+          <p className="text-sm text-gray-500">
+            Don't have an account?{' '}
+            <a href="/register" className="text-orange-600 hover:text-orange-500">Sign up</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
